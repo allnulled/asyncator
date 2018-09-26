07d20491b9041ea0b808523e0caa536cb7a6dbd0
allnulled <todosanulados@gmail.com>
2018-09-26 09:45:16 +0200

    Updated documentation and added "upload" file.

--- dev/upload.js
+++
@@ -1,5 +0,0 @@
-const exec = require("execute-command-sync");
-exec("bash dev/gitano.sh", {cwd: __dirname + "/.."});
-exec("gitanoaddfile README.md package.json src/* test/* dist/* dev/* && gitanopush", {
-	cwd: __dirname + "/../"
-});
